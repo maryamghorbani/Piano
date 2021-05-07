e83cc3873a29911acd9df68c789fc5adf1013c11
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-05-07 12:49:35 +0430

    add key nots sets

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const note = document.querySelector('.nowplaying');
+
+
 window.addEventListener('keydown', function (e) {
-    console.log(e);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+    if (!key) return;
+
+    const keyNote = key.getAttribute('data-note');
+    console.log(keyNote);
+
+    note.innerHTML = keyNote;
+
 });
